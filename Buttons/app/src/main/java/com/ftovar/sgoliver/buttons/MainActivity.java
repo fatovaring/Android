319c75d1d11f3713098509f02e204d7a52a585ca
Francisco Tovar <fatovaring@hotmail.com>
2016-07-15 18:14:25 -0500

    Buttons Job

--- Buttons/app/src/main/java/com/ftovar/sgoliver/buttons/MainActivity.java
+++ Buttons/app/src/main/java/com/ftovar/sgoliver/buttons/MainActivity.java
@@ -2,12 +2,43 @@ package com.ftovar.sgoliver.buttons;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button vbtnSimpleButton;
+    private TextView vlblMessage;
+    private ToggleButton vbtnToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        vbtnSimpleButton = (Button)findViewById(R.id.btnSimpleButton);
+        vlblMessage = (TextView)findViewById(R.id.lblMessage);
+        vbtnToggle = (ToggleButton)findViewById(R.id.btnToggle);
+
+        vbtnSimpleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vlblMessage.setText("Simple Button has been clicked");
+            }
+        });
+
+        vbtnToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(vbtnToggle.isChecked()){
+                    vlblMessage.setText("Toggle: ON");
+                }
+                else{
+                    vbtnToggle.setText("Toggle: OFF");
+                }
+            }
+        });
     }
 }
