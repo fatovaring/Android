cc9c9fe13be5bb41c3fd0c7986054f92d0b3ca25
Francisco Tovar <fatovaring@hotmail.com>
2016-07-19 10:13:46 -0500

    Checkbox and radiobutton

--- CheckBoxRadioButton/app/src/main/java/com/ftovar/sgoliver/checkboxradiobutton/MainActivity.java
+++ CheckBoxRadioButton/app/src/main/java/com/ftovar/sgoliver/checkboxradiobutton/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -13,6 +15,11 @@ public class MainActivity extends AppCompatActivity {
 
     private CheckBox vChkItem;
     private TextView vTxtView;
+    private RadioGroup vRadioGroup;
+    private RadioButton vOptUno;
+    private RadioButton vOptDos;
+    private TextView vLblRadioChange;
+    private TextView vLblRadioClick;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +54,63 @@ public class MainActivity extends AppCompatActivity {
                 else {
                     vTxtView.setText("Change!: Control unchecked");
                 }
+            }
+        });
+
+        //RadioGroup
+        vRadioGroup = (RadioGroup) findViewById(R.id.rdGroup);
+        vOptUno = (RadioButton) findViewById(R.id.optUno);
+        vOptDos = (RadioButton) findViewById(R.id.optDos);
+        vLblRadioChange = (TextView) findViewById(R.id.lblChange);
+        vLblRadioClick = (TextView) findViewById(R.id.lblClicked);
+
+        vRadioGroup.clearCheck();;
+        vRadioGroup.check(R.id.optDos);
+        vLblRadioClick.setText("Selected:" + vRadioGroup.getCheckedRadioButtonId());
+
+        //RADIO: Onclick (Options)
+        View.OnClickListener vListenerClick = new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                String vOption = "";
+
+                switch (view.getId()){
+                    case R.id.optUno:{
+                        vOption = "Option 1";
+                        break;
+                    }
+                    case R.id.optDos:{
+                        vOption = "Option 2";
+                        break;
+                    }
+                }
+
+                vLblRadioClick.setText("Clicked: " + vOption);
+            }
+        };
+
+        //Set listener
+        vOptUno.setOnClickListener(vListenerClick);
+        vOptDos.setOnClickListener(vListenerClick);
+
+        //RADIO: Onchange
+        vRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                String vOption = "";
+
+                switch (checkedId){
+                    case R.id.optUno:{
+                        vOption = "Option 1";
+                        break;
+                    }
+                    case R.id.optDos:{
+                        vOption = "Option 2";
+                        break;
+                    }
+                }
+
+                vLblRadioChange.setText("Change: " + vOption);
             }
         });
     }
